8444d16b098652948c48871c98f1f3e9c7f7b939
YuraLapin <88285853+YuraLapin@users.noreply.github.com>
2021-10-28 18:14:34 +0500

    Add files via upload

---
+++ Олимпиадные задачи/Олимпиадная задача №1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{    
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    int a, b, c;   
+
+    cout << "Введите трехзначное число\n";
+    cin >> x;
+
+    while ((x < 100) || (x > 999))
+    {
+        cout << "Введенное число не трехзначное, повторите ввод\n";
+        cin >> x;
+    }
+
+    a = x / 100;
+    b = (x / 10) % 10;
+    c = x % 10;
+
+    cout << "\nВсе возможные последовательности чисел:\t";
+
+    cout << a << b << c << "\t";    
+    cout << a << c << b << "\t";    
+    cout << b << a << c << "\t";    
+    cout << b << c << a << "\t";    
+    cout << c << a << b << "\t";    
+    cout << c << b << a << "\t";
+
+    if ((a > b) && (a > c))
+    {
+        cout << "\n\nНаибольшее из этих чисел: " << a;
+
+        if (b > c)
+        {
+            cout << b << c;
+        }
+        else
+        {
+            cout << c << b;
+        }
+    }
+    else
+    {
+        if ((b > a) && (b > c))
+        {
+            cout << "\n\nНаибольшее из этих чисел: " << b;
+
+            if (a > c)
+            {
+                cout << a << c;
+            }
+            else
+            {
+                cout << c << a;
+            }
+        }
+        else
+        {           
+            cout << "\n\nНаибольшее из этих чисел: " << c;
+
+            if (b > a)
+            {
+                cout << b << a;
+            }
+            else
+            {
+                cout << a << b;
+            }                        
+        }
+    } 
+    return 0;
+}
---
+++ Олимпиадные задачи/Олимпиадная задача №2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <clocale>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int attempts;
+    int num;
+    int usr_ans;
+    bool flag = false;
+
+    attempts = 7;
+
+    srand(time(NULL));
+    num = rand() % 100 + 1;     
+
+    cout << "Попробуйте угадать загаданное число. У вас " << attempts << " попыток.\n";    
+
+    while (attempts > 0)
+    {   
+        cin >> usr_ans;
+
+        attempts--;
+
+        if (usr_ans > num)
+        {            
+            cout << "Загаданное число меньше.";
+        }
+        if (usr_ans < num)
+        {            
+            cout << "Загаданное число больше.";
+        }
+        if (usr_ans == num)
+        {
+            cout << "Вы угадали! Число было равно " << num << ".";
+            flag = true; 
+            attempts = 0;
+        }        
+        else if (attempts != 0)
+        {
+            cout << " Попыток осталось " << attempts << "\n";
+        }
+    }
+    if (flag == false)
+    {
+        cout << "\nВы не угадали! Число было равно " << num << ". Попытки кончились.\n";
+    }  
+    return 0;
+}
